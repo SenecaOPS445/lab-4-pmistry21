e625456525aaf145e414ec87e9f1b54faca1fcee
Prasad Mistry <pmistry21@myseneca.ca>
2024-06-06 18:44:24 -0400

    Added lab4c

---
+++ lab4c.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Author Name: Prasad Mistry
+# Author ID: pmistry21@myseneca.ca
+
+# Description:  This program will create dictionaries, extract data from dictionaries, 
+# and to make comparisons between dictionaries.
+
+# Dictionaries.
+dict_york = {'Address': '70 The Pond Rd', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M3J3M6', 'Province': 'ON'}
+dict_newnham = {'Address': '1750 Finch Ave E', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M2J2X5', 'Province': 'ON'}
+
+# Lists.
+list_keys = ['Address', 'City', 'Country', 'Postal Code', 'Province']
+list_values = ['70 The Pond Rd', 'Toronto', 'Canada', 'M3J3M6', 'ON']
+
+def create_dictionary(keys, values):
+    # Place code here - refer to function specifics in section below.
+    i = 0
+    dictionary = {}
+    while i != len(keys):
+        dictionary[keys[i]] = values[i]
+        i = i + 1
+    return dictionary
+
+def shared_values(dict1, dict2):
+    sv1 = set(dict1.values())
+    sv2 = set(dict2.values())
+    return sv1.intersection(sv2)
+
+# Main program.
+if __name__ == '__main__':
+    york = create_dictionary(list_keys, list_values)
+    print('York: ', york)
+    common = shared_values(dict_york, dict_newnham)
+    print('Shared Values', common)
