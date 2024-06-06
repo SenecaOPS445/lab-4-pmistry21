7ab45c02222f63e5419533235c535b7925a54711
Prasad Mistry <pmistry21@myseneca.ca>
2024-06-06 19:16:55 -0400

    Added lab4d

--- lab4d.py
+++ lab4d.py
@@ -26,7 +26,7 @@ def middle_number(str1):
     return str1[1] + str1[2]
 
 def first_three_last_three(str1, str2):
-    # Grts the first and last three strings.
+    # Gets the first and last three strings.
     str1 = str(str1)
     str2 = str(str2)
     return str1[:3] + str2[-3:]
